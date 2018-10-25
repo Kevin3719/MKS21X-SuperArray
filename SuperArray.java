78f822987769bcc794aed17ff2c409b1532d9d7e
Kevin Chen <kchen01@stuy.edu>
2018-10-25 17:25:08 -0400

    nothing saved from earlier fml. Added variables.

---
+++ SuperArray.java
@@ -0,0 +1,21 @@
+public class SuperArray{
+
+  // the length of the array inside.
+  private int size;
+
+  // the data inside the SuperArray.
+  private String[] data;
+
+  // 0 Question 0 - constuctors
+  // starts with 10 null by default.
+  public SuperArray() {
+    size = 0;
+    data = new String[10];
+  }
+
+
+
+
+
+
+}
